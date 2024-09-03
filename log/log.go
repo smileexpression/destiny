e1df2b3a1916c1624d7ded9107841e0a3478a682
SmileExpression <15018135093@163.com>
2024-09-04 00:29:21 +0800

    update

---
+++ log/log.go
@@ -0,0 +1,29 @@
+package log
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var Logger *logrus.Logger
+
+func init() {
+	Logger = logrus.New()
+
+	// 设置日志格式为 JSON
+	Logger.SetFormatter(&logrus.JSONFormatter{})
+
+	// 设置日志级别为 Info
+	Logger.SetLevel(logrus.InfoLevel)
+
+	// 添加一个输出目标，比如 os.Stdout
+	file, err := os.OpenFile("log/logfile.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	Logger.SetOutput(file) // 将日志输出到文件
+
+	Logger.Info("Log initialized")
+}
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,1 +1,49 @@
 package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+
+	"smile.expression/destiny/log"
+	"smile.expression/destiny/pkg/database"
+)
+
+type App struct {
+	DBOptions *database.Options `json:"dbOptions"`
+}
+
+func (a *App) init() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(workDir + "/config")
+
+	// 读取配置文件
+	if err := viper.ReadInConfig(); err != nil {
+		log.Logger.WithError(err).Error("read config failed")
+	}
+
+	if err := viper.Unmarshal(&a); err != nil {
+		log.Logger.WithError(err).Error("unmarshal config file error")
+	}
+}
+
+func init() {
+	app := &App{
+		DBOptions: &database.Options{},
+	}
+	app.init()
+
+	viper.WatchConfig() // 监视配置文件的变化
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		// 在配置文件发生更改时重新加载配置
+		app.init()
+	})
+
+	db := database.NewDB(app.DBOptions)
+
+	defer fmt.Println(db)
+}
